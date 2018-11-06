fcbf0f8299bfedb118293bb76287f8495c29af99
ryanparker033 <43572925+ryanparker033@users.noreply.github.com>
2018-11-06 17:10:25 +0000

    Week 1+2+3
    All files from weeks 1+2+3

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefabTospawn;
+    public float adjustmentAngle = 0;
+
+    public void spawn()
+    {
+        Vector3 rotationInDegrees = transform.eulerAngles;
+        rotationInDegrees.z += adjustmentAngle;
+
+        Quaternion rotationInRadians = Quaternion.Euler(rotationInDegrees);
+
+        Instantiate(prefabTospawn, transform.position, rotationInRadians);
+    }
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public GameObject bulletprefab;
+    public Transform bulletspawn;
+    public float fireTime = 0.5f;
+
+    private bool isFiring = false;
+    
+    private void SetFiring() {
+        isFiring = false;
+
+    }
+
+    private void Fire() {
+        isFiring = true;
+        Instantiate(bulletprefab, bulletspawn.position, bulletspawn.rotation);
+
+        if (GetComponent<AudioSource>() != null) {
+            GetComponent<AudioSource>().Play();
+
+        }
+        Invoke("SetFiring", fireTime);
+    }
+    
+	private void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            if (!isFiring) {
+                Fire();
+            }
+        }
+	}
+}
